422d45a9427be725399d8284ac8338353b964813
Najmuddin Ahmad <98642530+NajmuddinAhmad@users.noreply.github.com>
2024-11-16 19:08:59 +0530

    Update UserDao.java

--- src/main/Java/com/users/UserDao.java
+++ src/main/Java/com/users/UserDao.java
@@ -1,5 +1,11 @@
 package com.users;
 
-public interface UserDao {
+import java.util.List;
 
+public interface UserDao {
+    void addUser(User user);
+    User getUserById(int userId);
+    List<User> getAllUsers();
+    void updateUser(User user);
+    void deleteUser(int userId);
 }
